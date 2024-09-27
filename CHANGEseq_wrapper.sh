9a8dad50579f45f0117a3c118a206151897489ab
ACTG802 <thudson686@gmail.com>
2024-09-27 13:02:16 -0700

    corrected wrapper path

--- CHANGEseq_wrapper.sh
+++ CHANGEseq_wrapper.sh
@@ -3,17 +3,22 @@
 #SBATCH -o %j.out
 #SBATCH -e %j.err
 
+
 ## ACTIVATE CONDA
 eval "$(conda shell.bash hook)"
-conda activate cseq
+conda activate cseq3
 
+# Get the directory where the script is located
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
+# Now you can reference this directory in your script
 echo "parameters manifest(csv): $1"
 echo "samples manifest(csv): $2"
 echo "command: $3"
 echo "sample: $4"
 echo "making yaml file"
-python /home/thudson/projects/IGU_CHANGEseq/make_yaml.py $1 $2
+
+python $SCRIPT_DIR/make_yaml.py $1 $2
 
 
 
@@ -21,6 +26,6 @@ infile=$2
 YAML=$(echo ${infile} | sed -e 's/\.csv$/.yaml/')
 echo $YAML
 
-python /home/thudson/projects/IGU_CHANGEseq/changeseq/changeseq.py $3 --manifest $YAML --sample $4
+python $SCRIPT_DIR/changeseq/changeseq.py $3 --manifest $YAML --sample $4
 
 
